b9c5ccf3fc12b519e34c9e40df3fb00bec36490b
Flames <isaacomenuche@gmail.com>
2023-11-28 23:24:31 +0100

    Checks for integer sign

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function that checks if a singly linked list has a cycle in it.* @list: list
+ * Return: 0 if there is no cycle, 1 if otherwise
+ */
+int check_cycle(listint_t *list)
+{
+
+}
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,3 @@
+# 0x01. Python - if/else, loops, functions
+
+## [0-positive_or_negative.py](./0-positive_or_negative.py)
