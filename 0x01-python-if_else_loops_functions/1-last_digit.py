1c1865e8535f9cd3d24703f2509f71a2f77ff95f
Flames <isaacomenuche@gmail.com>
2023-11-29 01:24:34 +0100

    Checks the last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -20,10 +20,10 @@ if number >= 0:
 
 else:
     if unsigned_last_digit > 5:
-        print("Last digit of {} is {} and is greater than 5".format
+        print("Last digit of {} is -{} and is greater than 5".format
               (number, unsigned_last_digit))
     elif (unsigned_last_digit < 6 and unsigned_last_digit != 0):
-        print("Last digit of {} is {} and is less than 6 and not 0".format
+        print("Last digit of {} is -{} and is less than 6 and not 0".format
               (number, unsigned_last_digit))
     else:
         print("Last digit of {} is {} and is 0".format
