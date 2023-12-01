c0a226e4427a030048b6a104dec2f9840add475a
Flames <isaacomenuche@gmail.com>
2023-12-01 02:43:44 +0100

    print args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,17 @@
 
 from sys import argv
 
-arg_count = len(argv) - 1
-if arg_count == 0:
-    print("{} arguments.".format(arg_count))
-elif arg_count == 1:
-    print("{} argument:".format(arg_count))
-    print("{}: {}".format(arg_count, argv[1]))
-else:
-    print("{} arguments:".format(arg_count))
-    for i in range(1, arg_count + 1):
-        print("{}: {}".format(i, argv[i]))
+def print_arg(argv):
+    arg_count = len(argv) - 1
+    if arg_count == 0:
+        print("{} arguments.".format(arg_count))
+    elif arg_count == 1:
+        print("{} argument:".format(arg_count))
+        print("{}: {}".format(arg_count, argv[1]))
+    else:
+        print("{} arguments:".format(arg_count))
+        for i in range(1, arg_count + 1):
+            print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    import sys
+    print_arg(sys.argv)
