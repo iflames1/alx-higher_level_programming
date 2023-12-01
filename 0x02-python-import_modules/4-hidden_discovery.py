1b2f6b382c188e5227d9ff91d1957b2584a63f44
Flames <isaacomenuche@gmail.com>
2023-12-01 10:42:39 +0100

    import magic

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from hidden_4 import *
+    import hidden_4
 
     name = dir(hidden_4)
     for i in name:
-        print("{:s}".format(i))
+        if i[:2] != '__':
+            print("{:s}".format(i))
