f3d71f2d099ef68d0a9927da30e6f9669ec9b96a
Flames <isaacomenuche@gmail.com>
2023-12-01 02:35:16 +0100

    script dynamic

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+arg_count = len(argv) - 1
+if arg_count == 0:
+    print("{} arguments.".format(arg_count))
+elif arg_count == 1:
+    print("{} argument:".format(arg_count))
+    print("{}: {}".format(arg_count, argv[1]))
+else:
+    print("{} arguments:".format(arg_count))
+    for i in range(1, arg_count + 1):
+        print("{}: {}".format(i, argv[i]))
