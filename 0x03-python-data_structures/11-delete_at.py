001d26988a2936c2bea6d19960ba095283b9ce7e
Flames <isaacomenuche@gmail.com>
2023-12-04 19:31:08 +0100

    switch

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,8 +3,9 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    pop_list = []
-    for i, value in enumerate(my_list):
+    pop_list = my_list[:]
+    my_list = []
+    for i, value in enumerate(pop_list):
         if i != idx:
-            pop_list.append(value)
-    return pop_list
+            my_list.append(value)
+    return my_list
