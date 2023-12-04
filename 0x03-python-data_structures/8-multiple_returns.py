4154356b1917290d90b1a72fee74449907f9562b
Flames <isaacomenuche@gmail.com>
2023-12-04 16:36:57 +0100

    ruturn multiple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence:
-        length = len(sentence)
-        first = sentence[0]
+        length, first = len(sentence), sentence[0]
         return (length, first)
