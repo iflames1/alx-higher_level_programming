ac9e3746c39e8cf2a6d1091fef5fae9c019df1cc
Flames <isaacomenuche@gmail.com>
2023-12-04 09:00:32 +0100

    print list in reverse

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-path = __import__('3-print_reversed_list_integer').print_reversed_list_integer
-print_reversed_list_integer = path
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
 my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    new_list = []
     for list in range(len(my_list), 0, -1):
-        print("{:d}".format(list))
+        new_list.append(list)
+    for n_list in range(0, len(new_list)):
+        print(new_list[n_list])
