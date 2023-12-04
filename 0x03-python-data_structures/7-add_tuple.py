5cbb118a7745d04833ee9a83e99f8b40dfd49d93
Flames <isaacomenuche@gmail.com>
2023-12-04 15:06:27 +0100

    add tupple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = ((tuple_a[0] if len(tuple_a) > 0 else 0) +
+                 (tuple_b[0] if len(tuple_b) > 0 else 0),
+                 (tuple_a[1] if len(tuple_a) > 1 else 0) +
+                 (tuple_b[1] if len(tuple_b) > 1 else 0))
+    return new_tuple
