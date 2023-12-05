4593de2ce0ae861834a6e376cb0815943f64c333
Flames <isaacomenuche@gmail.com>
2023-12-05 18:42:46 +0100

    check for palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,56 +1,66 @@
 #include "lists.h"
+#include <stdio.h>
 
-listint_t split_list(listint_t *fast, listint_t *slow)
+/**
+ * split_list - slipt a linked list
+ * @fast: fast pointer
+ * @slow: slow poiter
+ * Return: pointer to the start of the second line
+ */
+listint_t *split_list(listint_t *fast, listint_t *slow)
 {
-	listint_t *start_1, *start_2;
+	listint_t *start_2;
 
-	if (fast->next == NULL)
-	{
-		start_2 = slow->next->next;
-		return (*start_2);
-	}
-	if (fast == NULL)
+	if (fast == NULL || fast->next == NULL)
 	{
 		start_2 = slow->next;
-		return (*start_2);
+		slow->next = NULL;
+		return (start_2);
 	}
-	split_list(fast->next->next, slow->next);
+
+	return (split_list(fast->next->next, slow->next));
 }
 
+/**
+ * reverse - reverse a linked list
+ * @head: pointer to the hea
+ * Return: reverse linked list
+ */
 listint_t *reverse(listint_t *head)
 {
-	/* listint_t *prev = NULL, *current = head, *next = NULL; */
-	listint_t *p, *q;
+	listint_t *prev = NULL, *current = head, *next = NULL;
 
-	if (head == NULL || head->next == NULL)
-		return;
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
 
-	p = head;
-	q = p->next;
-
-	if (q == NULL)
-		return;
-
-	p->next = NULL;
-	q = reverse(q);
-	p->next->next = p;
-
-	return (q);
+	return (prev);
 }
 
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: pointer to the head node
+ * Return: 0 if its not a palindrome and 1 if otherwise
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *fast = *head, *slow = *head, *start_2;
+	listint_t *fast = *head, *slow = *head, *start_2 = NULL;
 
-	*start_2 = split_list(fast, slow);
+	start_2 = split_list(fast, slow);
 	start_2 = reverse(start_2);
 
-	while (start_2->next != NULL)
+	while (start_2 != NULL && *head != NULL)
 	{
-		if (start_2 != *head)
+		if (start_2->n != (*head)->n)
 		{
 			return (0);
 		}
+		start_2 = start_2->next;
+		*head = (*head)->next;
 	}
 
 	return (1);
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -21,6 +21,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-listint_t split_list(listint_t *fast, listint_t *slow);
+listint_t *split_list(listint_t *fast, listint_t *slow);
 
 #endif /* LISTS_H */
