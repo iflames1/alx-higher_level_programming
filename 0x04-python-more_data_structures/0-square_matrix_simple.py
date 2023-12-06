8838ffbcdca211a4adf27c76714daf3748210d7f
Flames <isaacomenuche@gmail.com>
2023-12-06 22:54:21 +0100

    returns the number of keys in a dictionary.

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/pythhon3
 
 def square_matrix_simple(matrix=[]):
-	new_matrix = [[i ** 2 for i in row] for row in matrix]
-	return new_matrix
+    new_matrix = [[i ** 2 for i in row] for row in matrix]
+    return new_matrix
