67342f789e1a01ce3476fd53bab8d5424d3e0374
Flames <isaacomenuche@gmail.com>
2023-12-06 20:21:28 +0100

    square value of all interger of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/pythhon3
 
 def square_matrix_simple(matrix=[]):
-    if matrix:
-        new_matrix = [[element ** 2 for element in row] for row in matrix]
-        return new_matrix
+	new_matrix = [[element ** 2 for element in row] for row in matrix]
+	return new_matrix
