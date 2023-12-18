f2ccc1a97f34e84b16da45bfec14cbb9712e8241
Flames <isaacomenuche@gmail.com>
2023-12-18 18:35:16 +0100

    divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,7 +6,8 @@ def list_division(my_list_1, my_list_2, list_length):
     try:
         for i in range(list_length):
             try:
-                if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
+                if (isinstance(my_list_1[i], (int, float))
+                        and isinstance(my_list_2[i], (int, float))):
                     new_list.append(my_list_1[i] / my_list_2[i])
                 else:
                     new_list.append(0)
