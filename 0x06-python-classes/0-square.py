13ee7589ce48dcff59097090504cb7e3e1912920
Flames <isaacomenuche@gmail.com>
2023-12-19 17:34:33 +0100

    Add documentation to square class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
 
 class Square:
+    """
+    This is the square class.
+
+    Attributes:
+        No attributes currently defined.
+
+    Methods:
+        No methods currently defined.
+    """
     pass
