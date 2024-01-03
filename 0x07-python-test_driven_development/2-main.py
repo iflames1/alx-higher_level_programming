e899278b65ee4531aca3277e517f4a56056d947a
Flames <isaacomenuche@gmail.com>
2024-01-03 23:23:30 +0100

    ZeroDivisionError

---
+++ 0x07-python-test_driven_development/2-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+sys.path.append('..')
+
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+print(matrix_divided(matrix, 3))
+print(matrix)
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" module for matrix_divided() method """
+
+
+def matrix_divided(matrix, div):
+    """ Divides two lists.
+
+    Args:
+        matrix: a list of two list of integer or float
+        div: the divisor
+    Raises:
+        TypeError: if matrix isn't list of list(s), if row in matrix isn't list
+            if div is not int or float.
+        ZeroDivisionError: if div is 0
+
+    Returns (int): Division of list in matrix
+    """
+    if not all(isinstance(row, list) and all(isinstance(num, (int, float))
+                                             for num in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if any(len(row) != len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    result = [[round(num / div, 2) for num in matrix]]
+
+    return result
