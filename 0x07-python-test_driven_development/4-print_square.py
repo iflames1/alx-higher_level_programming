804d2633eba2b9366102b4909012d01b4eaf666a
Flames <isaacomenuche@gmail.com>
2024-01-06 18:06:12 +0100

    Test driven development: print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" module for print_square method """
+
+def print_square(size):
+    """ prints a square with the character '#'.
+    Args:
+        size (int): the length of the square
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print('#' * size)
--- 0x07-python-test_driven_development/main_test_files/3-main.py
+++ 0x07-python-test_driven_development/main_test_files/3-main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import sys
+sys.path.append('..')
+
 say_my_name = __import__('3-say_my_name').say_my_name
 
 say_my_name("John", "Smith")
