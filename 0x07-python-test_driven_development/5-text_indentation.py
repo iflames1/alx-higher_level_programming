58e81b0b4283ff401682038c35615927717e3aa0
Flames <isaacomenuche@gmail.com>
2024-01-09 00:47:03 +0100

    TDD

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" module for text_indentation() method """
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    param = ['.', '?', ':']
+    index = 0
+    while index < len(text):
+        char = text[index]
+        if char in param:
+            print("\n\n", end="")
+            index += 1
+            while index < len(text) and text[index] == ' ':
+                index += 1
+
+        elif char != '\n':
+            print(char, end="")
+        index += 1
