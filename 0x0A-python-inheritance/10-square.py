3609612975a5acd86d6a470de5f84dad0a25fa06
Isaac <isaacomenuche@gmail.com>
2024-01-09 14:26:10 +0100

    Square class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
+""" Square class """
 
 
 class Square(Rectangle):
+    """Class representing a square."""
+
     def __init__(self, size):
         """Initialize a square instance.
 
@@ -12,7 +15,3 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-
-    def __str__(self):
-        """Return a string representation of the square."""
-        return "[Square] {}/{}".format(self.__size, self.__size)
