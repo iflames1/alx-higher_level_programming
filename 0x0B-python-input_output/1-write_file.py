3595c1ab265d99afc8c57d42cb8c87aed36a79f8
Flames <isaacomenuche@gmail.com>
2024-01-09 21:35:43 +0100

    1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a function write_file."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a text file (UTF8) and return the number of
+    characters written.
+
+    Args:
+        filename (str): The name of the text file.
+        text (str): The string to be written to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
+    with open(filename, 'w', encoding="utf=8") as file:
+        file.write(text)
