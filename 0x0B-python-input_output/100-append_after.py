b3fdb03bf77c37e620875b8668f20247645e411d
Flames <isaacomenuche@gmail.com>
2024-01-10 09:46:36 +0100

    status code

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file after each line containing a specific string.
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The specific string to search for in each line.
+        new_string (str): The line of text to insert after each line containing the search string.
+    """
+    lines = []
+    with open(filename, 'r') as file:
+        for line in file:
+            lines.append(line)
+            if search_string in line:
+                lines.append(new_string)
+
+    with open(filename, 'w') as file:
+        file.writelines(lines)
