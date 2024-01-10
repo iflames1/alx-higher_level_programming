a0f80ce8467c9d5f251b1e5d6c56d7b931e206ad
Flames <isaacomenuche@gmail.com>
2024-01-10 21:30:32 +0100

    TDD

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import numpy as np
+"""_summary_
+    """
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """_summary_
+
+    Args:
+        m_a (_type_): _description_
+        m_b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return (np.matmul(m_a, m_b))
