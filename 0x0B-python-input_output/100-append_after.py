f097efc622da216cc2794c7cac042fc21e4a2fcb
Flames <isaacomenuche@gmail.com>
2024-01-10 09:49:49 +0100

    code style

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -2,12 +2,14 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    Inserts a line of text to a file after each line containing a specific string.
+    Inserts a line of text to a file after each line containing
+    a specific string.
 
     Args:
         filename (str): The name of the file.
         search_string (str): The specific string to search for in each line.
-        new_string (str): The line of text to insert after each line containing the search string.
+        new_string (str): The line of text to insert after each line
+        containing the search string.
     """
     lines = []
     with open(filename, 'r') as file:
--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Reads stdin line by line and computes metrics.
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size>
 """
 
 import sys
