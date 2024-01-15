4ae4d54cf3c4cddb54f07b92e7123ec554694633
Flames <isaacomenuche@gmail.com>
2024-01-15 03:13:22 +0100

    Retangle class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.validate_integer("width", value)
+        self.validate_positive("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.validate_integer("height", value)
+        self.validate_positive("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.validate_integer("x", value)
+        self.validate_non_negative("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.validate_integer("y", value)
+        self.validate_non_negative("y", value)
+        self.__y = value
+
+    def validate_integer(self, attr_name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{attr_name} must be an integer")
+
+    def validate_positive(self, attr_name, value):
+        if value <= 0:
+            raise ValueError(f"{attr_name} must be > 0")
+
+    def validate_non_negative(self, attr_name, value):
+        if value < 0:
+            raise ValueError(f"{attr_name} must be >= 0")
