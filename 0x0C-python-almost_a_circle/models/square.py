e0d776d835f1e257c261cfa5060800e8f1d0fc7e
Flames <isaacomenuche@gmail.com>
2024-01-15 10:55:08 +0100

    Square dict representation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,5 +25,13 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
+    def to_dictionary(self):
+        return {
+            "id": self.id,
+            "size": self.size,
+            'x': self.x,
+            'y': self.y
+        }
+
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
