ba7bbc68e802c22c3bd964c41aeb19b00b914965
Flames <isaacomenuche@gmail.com>
2024-01-15 11:08:05 +0100

    dict to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ class Base """
-
+import json
 
 class Base:
     __nb_objects = 0
@@ -11,3 +11,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
