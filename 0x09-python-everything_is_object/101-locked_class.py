dae189f914bac0419408e3e74fca06929ae487a2
Flames <isaacomenuche@gmail.com>
2024-04-10 02:39:26 +0100

    __steattr__

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,10 +2,16 @@
 """ define a class LockedClass"""
 
 
-class LockedClass:
-    """ LockedClass"""
-    __slot__ = ["first_name"]
+from typing import Any
 
-    def __int__(self):
-        """ creates a new instances """
-        self.firstname = "first_name"
+
+class LockedClass:
+    """define a class LockedClass"""
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        """check if the attribute is first_name"""
+
+        if not hasattr(self, "first_name") and name != "first_name":
+            raise AttributeError("'LockedClass' object has no attribute "
+                                 "'{}'".format(name))
+        super().__setattr__(name, value)
