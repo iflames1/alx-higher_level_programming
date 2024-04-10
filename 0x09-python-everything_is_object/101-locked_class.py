7a168570fd7f05159233ea8c7f2ba14c81fac40a
Flames <isaacomenuche@gmail.com>
2024-04-10 02:41:22 +0100

    rm import Any

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,13 +2,10 @@
 """ define a class LockedClass"""
 
 
-from typing import Any
-
-
 class LockedClass:
     """define a class LockedClass"""
 
-    def __setattr__(self, name: str, value: Any) -> None:
+    def __setattr__(self, name: str, value) -> None:
         """check if the attribute is first_name"""
 
         if not hasattr(self, "first_name") and name != "first_name":
