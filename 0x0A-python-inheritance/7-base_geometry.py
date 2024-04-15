2ce4bb81bb3786a5dea7eb4632a2748d7a6610d9
Flames <isaacomenuche@gmail.com>
2024-04-15 14:53:08 +0100

    7-base_geometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,6 +20,6 @@ class BaseGeometry:
             ValueError: if value is less than or equal to 0
         """
         if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
