2aa9784b62ec1fc557d36cfcc4baa6a92c90e7c7
Flames <isaacomenuche@gmail.com>
2024-04-15 15:13:42 +0100

    use is operator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,10 +19,8 @@ class BaseGeometry:
             TypeError: if value is not an integer
             ValueError: if value is less than or equal to 0
         """
-        # if type(value) is not int:
-        #    raise TypeError("{} must be an integer".format(name))
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
