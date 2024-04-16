214ae554dd3089aafc6c26f942480c9921c5c4d8
Flames <isaacomenuche@gmail.com>
2024-04-16 14:07:23 +0100

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -12,8 +12,8 @@ class Square(Rectangle):
         Args:
             size (int): The size of the square.
         """
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
