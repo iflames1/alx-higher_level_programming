b1113b660a499318f526e9f3aae0b91e0573c851
Flames <isaacomenuche@gmail.com>
2024-04-17 11:50:50 +0100

    add permission

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Adds all command line arguments to a Python list
 and saves them to a file."""
+import os
 import sys
-from os.path import exists
-from json import load, dump
 
 # Importing custom functions from previous exercises
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -13,7 +12,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 
 # Create an empty list if the file doesn't exist
-if not exists(filename):
+if not os.path.exists(filename):
     my_list = []
 else:
     # Load the existing list from the file
@@ -24,3 +23,6 @@ my_list.extend(sys.argv[1:])
 
 # Save the updated list to the file
 save_to_json_file(my_list, filename)
+
+# Set permissions for the file
+os.chmod(filename, 0o600)
