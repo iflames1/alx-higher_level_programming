46bdf1df5fb57cabb853d2d75eb4b99f29ea1ebf
Flames <isaacomenuche@gmail.com>
2024-04-19 15:37:52 +0100

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 
 
 class Base:
@@ -27,3 +28,9 @@ class Base:
             raise ValueError("{} must be >= 0".format(name))
         if name == 'y' and value < 0:
             raise ValueError("{} must be >= 0".format(name))
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
