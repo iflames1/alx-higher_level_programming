03d1437d81121131b849efd4b094de19e8186789
Flames <isaacomenuche@gmail.com>
2024-04-19 16:21:29 +0100

    JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,8 +29,19 @@ class Base:
         if name == 'y' and value < 0:
             raise ValueError("{} must be >= 0".format(name))
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [objs.to_dictionary() for objs in list_objs]
+                file.write(cls.to_json_string(list_dicts))
