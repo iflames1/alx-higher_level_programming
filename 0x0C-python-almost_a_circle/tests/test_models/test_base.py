057780113bfb43e9fe24c97a35aebd2b83fe6801
Flames <isaacomenuche@gmail.com>
2024-04-23 00:36:35 +0100

    test id instance

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,11 +11,18 @@ class TestBase(unittest.TestCase):
     def test_id(self):
 
         b1 = Base()
+        self.assertIsInstance(b1.id, int)
+        self.assertTrue(b1.id > 0)
+
         self.assertEqual(b1.id, 1)
         b2 = Base()
         self.assertEqual(b2.id, 2)
         b3 = Base(12)
         self.assertEqual(b3.id, 12)
+
+        # Test if id is incrementing correctly
+        b4 = Base()
+        self.assertEqual(b4.id, b2.id + 1)
 
         # Test integer_validator method with valid inputs
         base = Base()
@@ -27,3 +34,8 @@ class TestBase(unittest.TestCase):
         with self.assertRaises(ValueError):
             base.integer_validator("width", 0)
 
+
+
+
+if __name__ == "__main__":
+    unittest.main()
