ee8dcb20b99aa062ecc142f1cf9a2b82b6665f52
Flames <isaacomenuche@gmail.com>
2024-05-22 14:56:07 +0100

    Temperatures #1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,10 +1,7 @@
--- Ensure you are using the correct database
-USE hbtn_0c_0;
-
 -- Display the top 3 cities' temperatures during July and August ordered by temperature in descending order
 SELECT
     city,
-    AVG(temperature) AS avg_temp
+    AVG(value) AS avg_temp
 FROM
     temperatures
 WHERE
