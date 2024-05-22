719c9e0ac6798da3de63e24f92b5fa8c06c431f0
Flames <isaacomenuche@gmail.com>
2024-05-22 12:56:45 +0100

    Full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- script that prints the full description of the table first_table from
 -- the database hbtn_0c_0 in your MySQL server.
-SELECT TABLE_NAME, COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.columns
-WHERE table_schema = DATABASE() AND table_name = 'first_table';
+SHOW CREATE TABLE first_table
