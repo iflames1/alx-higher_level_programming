8d54403d846161d09cb50888547244db8a1cce0b
Flames <isaacomenuche@gmail.com>
2024-05-22 14:20:36 +0100

    line width

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,5 @@
--- script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+-- script that creates a table second_table in the database hbtn_0c_0 in your
+-- MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
