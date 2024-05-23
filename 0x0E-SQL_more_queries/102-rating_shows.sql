241b93caaa331d1c08f9eed5794ff2728ba02e3f
Flames <isaacomenuche@gmail.com>
2024-05-23 14:33:11 +0100

    fix typo

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 --  script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 SELECT
     tv_shows.title,
-    SUM(tv_show_ratings.rate) AS rating_sum
+    SUM(tv_show_ratings.rate) AS rating
 FROM
     tv_shows
 JOIN
