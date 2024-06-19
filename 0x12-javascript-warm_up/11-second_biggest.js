dfff8bbf6ab4d146f13eed76b3d0243a18144fa1
Flames <isaacomenuche@gmail.com>
2024-06-19 23:22:22 +0100

    try

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,5 +5,6 @@ const args = process.argv.slice(2);
 if (args.length < 2) {
   console.log(0);
 } else {
-  console.log(args.sort()[args.length - 2]);
+  args.sort();
+  console.log(args.reverse()[1]);
 }
