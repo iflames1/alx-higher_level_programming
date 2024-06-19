bc98deba5d06c4e9ae0832eaae9dc42b5fafe01f
Flames <isaacomenuche@gmail.com>
2024-06-19 19:04:37 +0100

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const stringArr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const stringArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < stringArr.length; i++) {
   console.log(stringArr[i]);
