62c6173399e3efc39dc2efd3cac15cbe330f8997
Flames <isaacomenuche@gmail.com>
2024-06-23 01:11:39 +0100

    update doc

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Script that prints the State object with the name passed as an argument from
-the database hbtn_0e_6_usa.
+Script that adds the State object “Louisiana” to the database hbtn_0e_6_usa.
 """
 
 import sys
