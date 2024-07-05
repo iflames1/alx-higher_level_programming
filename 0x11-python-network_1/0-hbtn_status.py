50b61e6f7713d9f9f85f164d75f5fbd41affcb8b
Flames <isaacomenuche@gmail.com>
2024-07-05 19:49:58 +0100

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print('Body response:')
+print('\t- type: {}'.format(type(body)))
+print('\t- content: {}'.format(body))
+print('\t- utf8 content: {}'.format(body.decode('utf-8')))
