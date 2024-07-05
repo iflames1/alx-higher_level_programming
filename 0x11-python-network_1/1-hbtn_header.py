cc556f3072ea3563db3add39771f8ad43ea62fe0
Flames <isaacomenuche@gmail.com>
2024-07-05 20:17:24 +0100

    Response header value #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays the
+value of the X-Request-Id variable found in the header of the response."""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        headers = response.headers
+        x_request_id = headers.get("X-Request-Id")
+        print(x_request_id)
