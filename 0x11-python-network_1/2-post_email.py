dd92f5a94b8bda6d504fd006aa6f0e9b0ead0ce4
Flames <isaacomenuche@gmail.com>
2024-07-05 20:37:37 +0100

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response.
+"""
+
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+	url = sys.argv[1]
+	email = sys.argv[2]
+
+	data = urllib.parse.urlencode({'email': email})
+
+
+	with urllib.request.urlopen(url, data.encode('ascii')) as response:
+		body = response.read()
+		print(body.decode('utf-8'))
